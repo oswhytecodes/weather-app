e9de01ea1925318077409d71e9c875579ea77c2b
oswhyteknits <orincywhyte@gmail.com>
2022-08-30 18:29:30 -0400

    building up the backend for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000
 const express = require("express");
+const path = require('path')
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
@@ -8,7 +9,9 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather";
 const apiKEY = process.env.VITE_APP_WEATHER_API_KEY;
 
 app.use(cors());
-
+// Serve Node the files for build
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
+// api call
 app.get("/:city", async (req, res, next) => {
   const city = req.params.city;
   const url = `${apiURL}?q=${city}&appid=${apiKEY}&units=imperial`;
@@ -25,6 +28,10 @@ app.get("/:city", async (req, res, next) => {
     next(err);
   }
 });
+// unhandle requests get sent back to react
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+})
 
 app.listen(PORT, () => {
   console.log("Server started on port 8000");
