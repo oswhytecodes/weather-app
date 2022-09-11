57d6fac0c521dae48303c86b8f1d872e1a5fd5e4
oswhyteknits <orincywhyte@gmail.com>
2022-09-10 20:09:13 -0400

    added access log, performance check

--- server/server.js
+++ server/server.js
@@ -1,17 +1,35 @@
 require("dotenv").config();
-const PORT = process.env.PORT || 8000;
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const axios = require("axios");
-const apiKEY = process.env.VITE_APP_WEATHER_API_KEY;
-
+const express = require("express"),
+  app = express(),
+  PORT = process.env.PORT || 8000,
+  cors = require("cors"),
+  axios = require("axios"),
+  { performance } = require("perf_hooks"),
+  fs = require("fs"),
+  apiKEY = process.env.VITE_APP_WEATHER_API_KEY;
 app.use(cors());
+
+const now = new Date();
+const currentTime = `${now.getHours()}:${now.getMinutes()}`;
+// rate-limiting
+const userData = {};
+const timeBeforeApiCall = new Date().getMinutes();
+// caching
+const cityName = {};
+console.log("Current time : " + currentTime, timeBeforeApiCall);
 // api call for weather data
 app.get("/:city", async (req, res, next) => {
+  // api
   const apiURL = "https://api.openweathermap.org/data/2.5/weather";
   const city = req.params.city;
   const url = `${apiURL}?q=${city}&appid=${apiKEY}&units=imperial`;
+  // rate-limiting
+  const ip = req.socket.remoteAddress;
+  const timeOfApiCall = new Date().getMinutes();
+
+  // performance check
+  const performanceStartTime = performance.now();
+
   try {
     const result = await axios.get(url, {
       method: "GET",
@@ -20,11 +38,46 @@ app.get("/:city", async (req, res, next) => {
         "API-KEY": apiKEY,
       },
     });
-    res.json(result.data);
+    // api call
+    res.json({
+      main: {
+        temp: result.data.main.temp,
+        feels_like: result.data.main.feels_like,
+        humidity: result.data.main.humidity,
+      },
+      message: result.data.message,
+      cod: result.data.cod,
+      weather: result.data.weather,
+      name: result.data.name,
+      id: result.data.id,
+      dt: result.data.dt,
+      sys: {
+        sunrise: result.data.sys.sunrise,
+        sunset: result.data.sys.sunset,
+      },
+      timezone: result.data.timezone,
+    });
   } catch (err) {
     next(err);
   }
+
+  const performanceEndTime = performance.now();
+  // performance calculation
+  const performanceTime = (performanceEndTime - performanceStartTime).toFixed(
+    4
+  );
+  const line = `\n[User ${ip} made a call for ${city} at ${timeOfApiCall}. Data came back in ${performanceTime} milliseconds]`;
+
+  // access log
+  fs.appendFile("./access-log.txt", line, (err) => {
+    if (err) {
+      console.log("There is an error");
+    } else {
+      console.log(line);
+    }
+  });
 });
+
 // weather call for geocode data
 app.get("/:citycode", async (req, res, next) => {
   const apiURL = "http://api.openweathermap.org/geo/1.0/direct";
